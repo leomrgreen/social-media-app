9b012023c63ea8db7e5195ec3614ca623f9f7d43
Leo Jonsson <leojon00003@stud.noroff.no>
2024-11-05 11:00:22 +0100

    need new solution for mobile nav

--- components/ui/mobile-nav.jsx
+++ components/ui/mobile-nav.jsx
@@ -15,11 +15,6 @@ import Search from "../actions/search";
 import * as storage from "@/lib/utilities/storage";
 
 const MobileNav = () => {
-  const token = storage.load("token");
-  console.log(token);
-
-  if (!token) return null;
-
   return (
     <>
       <span className="fixed top-3 right-3 z-50 mobile-nav ">
